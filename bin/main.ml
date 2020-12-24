7166aebb3c57ce6d8b77ceec73c08daae81a2252
wasuken <wasuken1@gmail.com>
2020-12-24 09:44:19 +0900

    impl parsing file.
    next saving db.

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,14 @@
 open Core_kernel
 
+let indexing path =
+  let contents = In_channel.read_all path in
+  let m = Mecab.Tagger.create [|""|] in
+  let nodes = Mecab.Tagger.sparse_tonode m contents in
+  let filteredNodes = List.filter
+                        ~f:(fun node -> String.length node.surface > 2)
+                        nodes in
+  List.iter ~f:(fun node -> print_endline node.surface) filteredNodes
+
 let fileContentsSearch ptn path =
   let l = Core_kernel.In_channel.read_lines path
   and r = Str.regexp ptn in
@@ -10,6 +19,9 @@ let singleFileSearch ptn path =
   List.iter ~f:(fun x -> print_endline x) lines
 
 let () =
-  let ptn = Sys.argv.(1) in
-  List.iter ~f:(fun path -> printf "[%s]\n" path; singleFileSearch ptn path)
-    (List.drop (Array.to_list Sys.argv) 2)
+  let cmd = Sys.argv.(1)
+  and query = Sys.argv.(2) in
+  if (String.compare cmd "index") = 0 then indexing Sys.argv.(2)
+  else if (String.compare cmd "index") = 0 then List.iter
+                                                  ~f:(fun path -> printf "[%s]\n" path; singleFileSearch query path)
+                                                  (List.drop (Array.to_list Sys.argv) 3)
